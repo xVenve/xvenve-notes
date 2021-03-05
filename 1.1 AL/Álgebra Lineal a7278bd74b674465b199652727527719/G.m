5ba578a0da75e083dca3918dabeb712c46c10d76
Jorge Rodríguez <jrf1616@gmail.com>
2021-03-05 15:02:02 +0100

    AL 1.2
    Former-commit-id: 2380c1596cb0e9f6bc818cf0402cf8bd6a5ef5b4

--- 1.1 AL/Álgebra Lineal a7278bd74b674465b199652727527719/G.m
+++
@@ -1,2 +0,0 @@
-function G v1 = input('Dame el vector v1'); v2 = input('Dame el vector v2'); v3 = input('Dame el vector v3'); A=[v1;v2;v3]; while(det(A)==0) disp('Estos vectores no son linealmente independientes'); v1 = input('Dame el vector v1'); v2 = input('Dame el vector v2'); v3 = input('Dame el vector v3'); A=[v1;v2;v3]; end u1=v1; u2= v2-(dot(v2,u1)/norm(u1)^2)u1; u3= v3-(dot(v3,u2)/norm(u2)^2)u2-(dot(v3,u1)/norm(u1)^2)*u1; disp(strcat('El vector u1 es', mat2str(u1))); disp(strcat('El vector u2 es', mat2str(u2))); disp(strcat('El vector u3 es', mat2str(u3))); end
-Q=[1 2;-1 -5]
--- 1.1 AL/Álgebra Lineal a7278bd74b674465b199652727527719/Untitled.m
+++
@@ -1,7 +0,0 @@
-m=[41 32;21 65];
-b=[9;8];
-Traza=trace(m)
-Norma=norm(b)
-sqrt(25)
-Polinomio=[1 -6];
-Raices= roots(Polinomio)
