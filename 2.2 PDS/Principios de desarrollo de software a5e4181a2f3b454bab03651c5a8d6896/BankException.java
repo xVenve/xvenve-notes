17e5a1927e7696592efdeb070bc1fd1d2075c184
Jorge Rodríguez <jrf1616@gmail.com>
2021-03-05 15:00:58 +0100

    PDS 1.1
    Former-commit-id: b765a87e70d80e4d857b48c064fad3f1787b6d99

--- 2.2 PDS/Principios de desarrollo de software a5e4181a2f3b454bab03651c5a8d6896/BankException.java
+++
@@ -1,9 +0,0 @@
-package bank.logic;
-
-public class BankException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public String toString() {
-		return "Insufficient funds in the source account.";
-	}
-}
