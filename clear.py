60d58bd5a21ed4b91a418c0a30ccca1077e3affe
Jorge Rodríguez <jrf1616@gmail.com>
2021-06-09 17:29:25 +0200

    Clear repo for releases 1.0 #65
    Former-commit-id: b83c3b948f3c34c5d787d9e1a445885c8717f379

---
+++ clear.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+from sys import argv
+
+root = os.listdir(".")
+
+for r in root:
+    if r.find(".") != 0:
+        if r.find("Course") == -1 and r != "clear.py":
+            if os.path.isfile(r) or os.path.islink(r):
+                os.remove(r)
+                print(r+" Removed!")
+                continue
+            elif os.path.isdir(r):
+                shutil.rmtree(r)
+                print(r+" Removed!")
+                continue
+        elif r != "clear.py":
+            course = os.listdir(r)
+            for c in course:
+                if c.find(".") == -1:
+                    pathc = r+"/"+c
+                    subject = os.listdir(pathc)
+                    for s in subject:
+                        if s.find(".") == -1:
+                            file = os.listdir(pathc+"/"+s)
+                            for f in file:
+                                pathf = r+"/"+c+"/"+s+"/"+f
+                                if f.find(".pdf") == -1:
+                                    if os.path.isfile(pathf) or os.path.islink(pathf):
+                                        os.remove(pathf)
+                                        print(pathf+" Removed!")
+                                    elif os.path.isdir(pathf):
+                                        shutil.rmtree(pathf)
+                                        print(pathf+" Removed!")
+                                else:
+                                    print(pathf, pathc+"/"+f)
+                                    shutil.move(pathf, pathc+"/"+f)
+                        if os.path.isdir(pathc+"/"+s):
+                            shutil.rmtree(pathc+"/"+s)
+                            print(pathc+"/"+s+" Removed!")
+
+remove(argv[0])
